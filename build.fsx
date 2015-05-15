ca7f927e2c532955447f27e7c5889f601013ae66
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-05-15 14:34:46 +0200

    Get version from WebSharper

--- build.fsx
+++ build.fsx
@@ -2,8 +2,9 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("WebSharper.SlickGrid", "3.0")
-    |> fun bt -> bt.WithFramework(bt.Framework.Net40)
+    BuildTool().PackageId("WebSharper.SlickGrid")
+        .VersionFrom("WebSharper")
+        .WithFramework(fun fw -> fw.Net40)
 
 let resourceFiles =
     [
